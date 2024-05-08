f774fb8296276a67f9165bb0362c2a2622794f14
Hideki <165275663+CayoYoshimura@users.noreply.github.com>
2024-05-08 17:29:59 -0300

    Update Ex11_Calculo.py

---
+++ Ex11_Calculo.py
@@ -0,0 +1,11 @@
+numero1 = int(input("Digite um número: "))
+numero2 = int(input("Digite um número: "))
+numero3 = int(input("Digite um número: "))
+
+calculo1 = (numero1 * 2) + (numero2 / 2) # O produto do dobro do primeiro com metade do segundo
+calculo2 = (numero1 * 3) + (numero3) # A soma do triplo do primeiro com o terceiro
+calculo3 = (numero3**3) # O terceiro elevado ao cubo
+
+print(calculo1)
+print(calculo2)
+print(calculo3)
