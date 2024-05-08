cacae79fee84372ea69bf5931c373f66bf1e5b3f
Hideki <165275663+CayoYoshimura@users.noreply.github.com>
2024-05-08 16:15:15 -0300

    Delete ex4.py

--- ex4.py
+++
@@ -1,6 +0,0 @@
-n1 = float(input("Digite a nota 1: "))
-n2 = float(input("Digite a nota 2: "))
-n3 = float(input("Digite a nota 3: "))
-n4 = float(input("Digite a nota 4: "))
-media=(n1+n2+n3+n4)/4
-print("A média do aluno:", media)
