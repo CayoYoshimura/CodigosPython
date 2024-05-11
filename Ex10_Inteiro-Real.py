c990b4ba7ff0f40857f06432a5800c37685cd303
Hideki <165275663+CayoYoshimura@users.noreply.github.com>
2024-05-11 19:38:39 -0300

    Atividade

--- Ex10_Inteiro-Real.py
+++ Ex10_Inteiro-Real.py
@@ -1,4 +1,4 @@
 numero1 = int(input("Digite um numero inteiro: "))
 numero2 = int(input("Digite um numero inteiro: "))
 numero3 = float(input("Digite um numero real: "))
-print(numero1, numero2, numero3, "\n")
+print(numero1, numero2, numero3)
