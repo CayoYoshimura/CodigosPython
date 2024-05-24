9e7da1ad435cde4576888d2edd06d41ab142940b
Hideki <165275663+CayoYoshimura@users.noreply.github.com>
2024-05-24 16:55:45 -0300

    Atividade

--- Ex07_for-Numeros-Pares-Impares.py
+++ Ex07_for-Numeros-Pares-Impares.py
@@ -2,9 +2,8 @@ num = 1
 par = impar = 0
 while num != 0:
     num = int(input("Digite um número: "))
-    if num != 0:
-        if num % 2 == 0:
-            par += 1
-        else:
-            impar += 1
-print("Você digite {} números pares e {} números impares".format(par, impar))
+    if num % 2 == 0:
+        par += 1
+    else:
+        impar += 1
+print("Você digitou {} números pares e {} números impares".format(par, impar))
